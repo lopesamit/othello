d5b6e18e8dcfd1c1373ef323fade83b92a464ca4
lopesamit <lopesamit@gmail.com>
2018-04-27 15:29:02 -0400

    Diagonally working too. Full game works well

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -79,7 +79,7 @@ const initialState = {
           } else if(state.arr[co[0]][i] === undefined){ 
             break;
           } else if(state.arr[co[0]][i] === false && i<7){ 
-            console.log(co[0]+" "+i)
+            //console.log(co[0]+" "+i)
             rCount++;
           }
         }
@@ -112,6 +112,107 @@ const initialState = {
             }
           }
         }
+
+        // diagonal down checking. click up and it will check for all diagonal blocks downwards to left /
+        let countDL = 0;
+        for(let i = 0; i<Number(co[1]);i++){
+          if(co[0]>=6 || co[1]<2) break;
+          if(state.arr[Number(co[0])+i+1][Number(co[1])-i-1] === true){ 
+            //console.log((Number(co[0])+i+1)+" " +(Number(co[1])-i-1)+ " break true")
+            break;
+          } else if(state.arr[Number(co[0])+i+1][Number(co[1])-i-1] === undefined){
+            //console.log((Number(co[0])+i+1)+" " +(Number(co[1])-i-1)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])+i+1][Number(co[1])-i-1] === false && i<7){ 
+            countDL++;
+          }
+        }
+        //console.log(countDL);
+        if(countDL>0){
+          for(let j = 0; j<countDL;j++){
+            if(state.arr[Number(co[0])+countDL+1][Number(co[1])-countDL-1] === true){
+              state.arr[Number(co[0])+countDL-j][Number(co[1])-countDL+j] = true;
+              //console.log((Number(co[0])+countDL-j)+" " +(Number(co[1])-countDL+j)+ " converted")
+            }
+          }
+        }
+
+        // diagonal down checking. click up and it will check for all diagonal blocks downwards to right \
+        let countDR = 0;
+        for(let i = 1; i<7;i++){
+          if(Number(co[1])+i >6 || Number(co[0])+i >6 ){ break; }
+          if(state.arr[Number(co[0])+i][Number(co[1])+i] === true){ 
+            //console.log((Number(co[0])+i)+" " +(Number(co[1])+i)+ " break true")
+            break;
+          } else if(state.arr[Number(co[0])+i][Number(co[1])+i] === undefined){
+            //console.log((Number(co[0])+i)+" " +(Number(co[1])+i)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])+i][Number(co[1])+i] === false){
+            //console.log((Number(co[0])+i)+" " +(Number(co[1])+i)+ " count")
+            countDR++;
+          }
+        }
+        //console.log(countDR);
+        if(countDR>0){
+          for(let j = 1; j<=countDR;j++){
+            if(state.arr[Number(co[0])+countDR+1][Number(co[1])+countDR+1] === true){
+              state.arr[Number(co[0])+j][Number(co[1])+j] = true;
+              //console.log((Number(co[0])+j)+" " +(Number(co[1])+j)+ " converted")
+            }
+          }
+        }
+
+        // diagonal down checking. click up and it will check for all diagonal blocks upwards to right \
+        let countUR = 0;
+        for(let i = 1; i<7;i++){
+          if(Number(co[0])-i < 1 || Number(co[1])-i >6 ){ console.log("breaked "); break; }
+          if(state.arr[Number(co[0])-i][Number(co[1])-i] === true){ 
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])-i)+ " break true")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])-i] === undefined){
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])-i)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])-i] === false){
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])-i)+ " count")
+            countUR++;
+          }
+        }
+        //console.log(countUR);
+        if(countUR>0){
+          for(let j = 1; j<=countUR;j++){
+            if(state.arr[Number(co[0])-countUR-1][Number(co[1])-countUR-1] === true){
+              state.arr[Number(co[0])-j][Number(co[1])-j] = true;
+              //console.log((Number(co[0])-j)+" " +(Number(co[1])-j)+ " converted")
+            }
+          }
+        }
+
+        // diagonal down checking. click up and it will check for all diagonal blocks upwards to left /
+        let countUL = 0;
+        for(let i = 1; i<7;i++){
+          if(Number(co[0])-i < 1 || Number(co[1])-i >6 ){ console.log("breaked "); break; }
+          if(state.arr[Number(co[0])-i][Number(co[1])+i] === true){ 
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])+i)+ " break true")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])+i] === undefined){
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])+i)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])+i] === false && i<7){ 
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])+i)+ " count")
+            countUL++;
+          }
+        }
+        //console.log(countUL);
+        if(countUL>0){
+          for(let j = 1; j<=countUL;j++){
+            if(state.arr[Number(co[0])-countUL-1][Number(co[1])+countUL+1] === true){
+              state.arr[Number(co[0])-j][Number(co[1])+j] = true;
+              //console.log((Number(co[0])+countDL-j)+" " +(Number(co[1])-countDL+j)+ " converted")
+            }
+          }
+        }
+
+
 
       }
 
@@ -152,10 +253,10 @@ const initialState = {
             countUp++;
           }
         }
-        console.log(countUp);
+        //console.log(countUp);
 
         if(countUp>0){
-          console.log(upY+countUp)
+          //console.log(upY+countUp)
           for(let j = 0; j<countUp;j++){
             if(state.arr[upY-countUp][co[1]] === false){
               state.arr[upY-j][co[1]] = false;
@@ -172,7 +273,7 @@ const initialState = {
           } else if(state.arr[co[0]][i] === undefined){ 
             break;
           } else if(state.arr[co[0]][i] === true && i<7){ 
-            console.log(co[0]+" "+i)
+            //console.log(co[0]+" "+i)
             rCount++;
           }
         }
@@ -206,6 +307,112 @@ const initialState = {
           }
         }
 
+        // diagonal down checking. click up and it will check for all diagonal blocks downwards to left /
+
+        let countDL = 0;
+        for(let i = 0; i<Number(co[1]);i++){
+          //console.log(" co[0] "+ (Number(co[0])+i) + " co[1] "+ (Number(co[1])-i) )
+          if(Number(co[0])+i >=6 || co[1]<2 ){ break; }
+          if(state.arr[Number(co[0])+i+1][Number(co[1])-i-1] === false){ 
+            //console.log((Number(co[0])+i+1)+" " +(Number(co[1])-i-1)+ " break false")
+            break;
+          } else if(state.arr[Number(co[0])+i+1][Number(co[1])-i-1] === undefined){
+            //console.log((Number(co[0])+i+1)+" " +(Number(co[1])-i-1)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])+i+1][Number(co[1])-i-1] === true && i<7){ 
+            //console.log((Number(co[0])+i+1)+" " +(Number(co[1])-i-1)+ " count")
+            countDL++;
+          }
+        }
+        console.log(countDL);
+        if(countDL>0){
+          for(let j = 1; j<=countDL;j++){
+            if(state.arr[Number(co[0])+countDL+1][Number(co[1])-countDL-1] === false){
+              state.arr[Number(co[0])+j][Number(co[1])-j] = false;
+              //console.log((Number(co[0])+j)+" " +(Number(co[1])-j)+ " converted")
+            }
+          }
+        }
+
+
+        // diagonal down checking. click up and it will check for all diagonal blocks downwards to right \
+        let countDR = 0;
+        for(let i = 1; i<7;i++){
+          if(Number(co[1])+i >6 || Number(co[0])+i >6 ){ break; }
+          if(state.arr[Number(co[0])+i][Number(co[1])+i] === false){ 
+            //console.log((Number(co[0])+i)+" " +(Number(co[1])+i)+ " break true")
+            break;
+          } else if(state.arr[Number(co[0])+i][Number(co[1])+i] === undefined){
+            //console.log((Number(co[0])+i)+" " +(Number(co[1])+i)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])+i][Number(co[1])+i] === true){
+            //console.log((Number(co[0])+i)+" " +(Number(co[1])+i)+ " count")
+            countDR++;
+          }
+        }
+        //console.log(countDR);
+        if(countDR>0){
+          for(let j = 1; j<=countDR;j++){
+            if(state.arr[Number(co[0])+countDR+1][Number(co[1])+countDR+1] === false){
+              state.arr[Number(co[0])+j][Number(co[1])+j] = false;
+              //console.log((Number(co[0])+j)+" " +(Number(co[1])+j)+ " converted")
+            }
+          }
+        }
+
+
+        // diagonal down checking. click up and it will check for all diagonal blocks upwards to right \
+        let countUR = 0;
+        for(let i = 1; i<7;i++){
+          if(Number(co[1])-i >6 || Number(co[0])-i < 1 ){ break; }
+          if(state.arr[Number(co[0])-i][Number(co[1])-i] === false){ 
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])-i)+ " break true")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])-i] === undefined){
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])-i)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])-i] === true){
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])-i)+ " count")
+            countUR++;
+          }
+        }
+        //console.log(countUR);
+        if(countUR>0){
+          for(let j = 1; j<=countUR;j++){
+            if(state.arr[Number(co[0])-countUR-1][Number(co[1])-countUR-1] === false){
+              state.arr[Number(co[0])-j][Number(co[1])-j] = false;
+              //console.log((Number(co[0])-j)+" " +(Number(co[1])-j)+ " converted")
+            }
+          }
+        }
+
+        // diagonal down checking. click up and it will check for all diagonal blocks upwards to left /
+        let countUL = 0;
+        for(let i = 1; i<7;i++){
+          if(Number(co[0])-i < 1 || Number(co[1])-i >6 ){ console.log("breaked "); break; }
+          if(state.arr[Number(co[0])-i][Number(co[1])+i] === false){ 
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])+i)+ " break true")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])+i] === undefined){
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])+i)+ " break undefined")
+            break;
+          } else if(state.arr[Number(co[0])-i][Number(co[1])+i] === true && i<7){ 
+            //console.log((Number(co[0])-i)+" " +(Number(co[1])+i)+ " count")
+            countUL++;
+          }
+        }
+        //console.log(countUL);
+        if(countUL>0){
+          for(let j = 1; j<=countUL;j++){
+            if(state.arr[Number(co[0])-countUL-1][Number(co[1])+countUL+1] === false){
+              state.arr[Number(co[0])-j][Number(co[1])+j] = false;
+              //console.log((Number(co[0])+countDL-j)+" " +(Number(co[1])-countDL+j)+ " converted")
+            }
+          }
+        }
+
+
+
       }
 
 
@@ -216,7 +423,6 @@ const initialState = {
         })
       }));
       if(count===64){
-        console.log("game over");
         alert("Game Over");
       }
        
